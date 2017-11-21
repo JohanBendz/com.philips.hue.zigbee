fba99b67f9af20b284d5d951d37064d2724648ed
Sebastian Johansson <johansson.sebastian@gmail.com>
2017-11-21 10:39:22 +0100

    Added support for Hue A19 bulb and restructured some of the files

--- drivers/LWB010/device.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
-
-class PhiliphsHueWhiteZigBee extends ZigBeeLightDevice {
-
-}
-
-module.exports = PhiliphsHueWhiteZigBee;
