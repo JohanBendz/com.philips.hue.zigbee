e727e407ee4134c3d29f93e151ea66c0390bcc78
Stig Salte Tidemandsen <tidemann@gmail.com>
2018-09-19 00:29:41 +0200

    Added support for Philips Being Ceiling Lamp (LTC001) & Philips Fair Ceiling Lamp (LTC002)

---
+++ drivers/LTC001/device.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
+const HueAmbiance = require('../HueAmbiance.js');
+
+class LTC001 extends HueAmbiance {
+
+}
+
+module.exports = LTC001;
---
+++ drivers/LTC002/device.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
+const HueAmbiance = require('../HueAmbiance.js');
+
+class LTC002 extends HueAmbiance {
+
+}
+
+module.exports = LTC002;
