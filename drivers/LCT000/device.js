30102339b7d4100b1c242b1dc1fb152100e3b357
Bram Oosterhuis <bram.oosterhuis.nl@gmail.com>
2020-07-28 09:52:28 +0200

    LCT000 clean up

--- drivers/LCT000/device.js
+++ drivers/LCT000/device.js
@@ -2,14 +2,6 @@
 
 const HueLight = require("../HueLight.js");
 
-class LCT000 extends HueLight {
-
-	async async onNodeInit({ zclNode }) {
-        
-        // this.printNode();
-
-    }
-
-}
+class LCT000 extends HueLight {}
 
 module.exports = LCT000;
