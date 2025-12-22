73a446f6fb2abc3afc6c1a16b9d3e4c7a4f0e66b
Christian Kuhn <lolli@schwarzbu.ch>
2025-12-22 18:54:10 +0100

    [BUGFIX] Working onOff and battery status for SOC001
    Update handling of the Contact Sensor SOC001 to
    correctly report onOff status based on cluster 6.
    Also implement battery percentage measurement.

--- drivers/SOC001/device.js
+++ drivers/SOC001/device.js
@@ -2,34 +2,45 @@
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
 
+//const {debug} = require('zigbee-clusters');
+//debug(true);
+
 class ContactSensor extends ZigBeeDevice {
 
   async onNodeInit({ zclNode }) {
     this.log('Philips Hue Contact Sensor has been initialized');
+    this.printNode();
 
-    // Register capabilities
-    this.registerCapability('alarm_contact', 'genOnOff');
-    this.registerCapability('measure_battery', 'genPowerCfg');
-
-    // Register attribute listeners
-    zclNode.endpoints[2].clusters.genOnOff
-      .on('attr.onOff', this.onContactAlarmAttributeReport.bind(this));
-    
-    zclNode.endpoints[2].clusters.genPowerCfg
-      .on('attr.batteryPercentageRemaining', this.onBatteryPercentageAttributeReport.bind(this));
+    // The device has custom cluster 64518 (0xfc06) which delivers contact and tamper.
+    // We currently implement cluster 6 instead. More information:
+    // https://github.com/Koenkk/zigbee-herdsman-converters/pull/9668
+    // https://github.com/zigpy/zha-device-handlers/pull/3432
+    // https://github.com/zigpy/zha-device-handlers/issues/3314
+    // https://github.com/dresden-elektronik/deconz-rest-plugin/issues/7226
+    const node = await this.homey.zigbee.getNode(this);
+    node.handleFrame = (endpointId, clusterId, frame, meta) => {
+      this.log("endpointId: ", endpointId, ", clusterId: ", clusterId, ", frame: ", frame, ", meta: ", meta);
+      if ((endpointId === 2)
+          && (clusterId === 6) // onOff cluster
+          && (frame.readUInt8(0) === 0x01) // attribute 1
+      ) {
+        // Buffer 01 3a 01
+        // value 0 = "closed", value 1 = "open" ?!
+        const value = frame.readUInt8(2);
+        this.setCapabilityValue('alarm_contact', value === 1);
+      }
+      if ((endpointId === 2)
+          && (clusterId === 1) // PowerConfigurationCluster
+          && (frame.readUInt8(2) == 0x0a)
+          && (frame.readUInt8(3) == 0x21) // 33 - "batteryPercentageRemaining"
+          && (frame.readUInt8(4) == 0x00)
+      ) {
+          const percentage = frame.readUInt8(6) / 2; // translate incoming values 0..200 to 0..100 percent
+          this.log("battery percentage remaining: ", percentage);
+          this.setCapabilityValue('measure_battery', percentage);
+      }
+    };
   }
-
-  onContactAlarmAttributeReport(value) {
-    this.log('Contact alarm attribute report received:', value);
-    this.setCapabilityValue('alarm_contact', value === 1);
-  }
-
-  onBatteryPercentageAttributeReport(value) {
-    const batteryPercentage = value / 2; // Convert from half percent to percent
-    this.log('Battery percentage attribute report received:', batteryPercentage);
-    this.setCapabilityValue('measure_battery', batteryPercentage);
-  }
-
 }
 
 module.exports = ContactSensor;
