ca108f91b18f58543d333eeb093cff95a74c2608
Johan Bendz <34488771+JohanBendz@users.noreply.github.com>
2026-09-20 16:30:43 +0200

    feat: add Lightguide Edison bulbs

---
+++ drivers/LCV001/device.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Light = require("../Light.js");
+
+class LCV001 extends Light { }
+
+module.exports = LCV001;
---
+++ drivers/LCV002/device.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const Light = require("../Light.js");
+
+class LCV002 extends Light { }
+
+module.exports = LCV002;
